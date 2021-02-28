a2b9711297972379d9457581e853606bcc18fda5
Qstick <qstick@gmail.com>
2021-02-28 01:03:44 -0500

    Fix Int types

--- parserr/parserr.py
+++ parserr/parserr.py
@@ -192,12 +192,10 @@ class Parserr(commands.Cog):
         quality_proper = "True" if parsed_obj["quality"]["revision"]["version"] > 1 else "-" or "-"
         quality_repack = "True" if parsed_obj["quality"]["revision"]["isRepack"] is True else "-" or "-"
 
-        raise Exception(parsed_obj)
-
         embed.add_field(name="Movie Title", value=parsed_obj.get("movieTitle", "-"), inline=True)
-        embed.add_field(name="Year", value=parsed_obj.get("year", "-"), inline=True)
+        embed.add_field(name="Year", value=parsed_obj["year"] or "-", inline=True)
         embed.add_field(name="Edition", value=parsed_obj.get("edition", "-"), inline=True)
-        embed.add_field(name="TMDBId", value=parsed_obj.get("tmdbId", 0), inline=True)
+        embed.add_field(name="TMDBId", value=parsed_obj["tmdbId"] or "-", inline=True)
         embed.add_field(name="IMDbId", value=parsed_obj.get("imdbId", "-"), inline=True)
         embed.add_field(name="Quality", value=quality, inline=False)
         embed.add_field(name="Proper", value=quality_proper, inline=True)
@@ -244,7 +242,7 @@ class Parserr(commands.Cog):
 
         embed.add_field(name="Author Name", value=parsed_obj.get("authorName", "-"), inline=True)
         embed.add_field(name="Book Title", value=parsed_obj.get("bookTitle", "-"), inline=True)
-        embed.add_field(name="Release Date", value=parsed_obj.get("releaseDate", "-"), inline=True)
+        embed.add_field(name="Release Date", value=parsed_obj["releaseDate"] or "-", inline=True)
         embed.add_field(name="Quality", value=quality, inline=False)
         embed.add_field(name="Proper", value=quality_proper, inline=True)
         embed.add_field(name="Real", value=quality_real, inline=True)
@@ -264,7 +262,7 @@ class Parserr(commands.Cog):
 
         embed.add_field(name="Artist Name", value=parsed_obj.get("artistName", "-"), inline=True)
         embed.add_field(name="Album Title", value=parsed_obj.get("albumTitle", "-"), inline=True)
-        embed.add_field(name="Release Date", value=parsed_obj.get("releaseDate", "-"), inline=True)
+        embed.add_field(name="Release Date", value=parsed_obj["releaseDate"] or "-", inline=True)
         embed.add_field(name="Discography", value=parsed_obj.get("discography", "-"), inline=True)
         embed.add_field(name="Quality", value=quality, inline=False)
         embed.add_field(name="Proper", value=quality_proper, inline=True)
