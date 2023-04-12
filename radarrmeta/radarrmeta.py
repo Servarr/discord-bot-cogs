3b795bf63a107065ecb156f4c2e786400b8604e9
Daniel Underwood <git@danielunderwood.dev>
2023-04-11 21:35:07 -0400

    refresh: Fix permissions, again

--- radarrmeta/radarrmeta.py
+++ radarrmeta/radarrmeta.py
@@ -84,7 +84,7 @@ class RadarrMeta(commands.Cog):
         self._headers = {'User-Agent': 'Python/3.8'}
 
     @commands.group(invoke_without_command=True)
-    @checks.mod_or_permissions(admin=True)
+    @checks.mod_or_permissions(administrator=True)
     async def refresh(self, ctx, *, resources: str):
         """
         Refreshes cached items
