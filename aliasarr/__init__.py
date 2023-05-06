4f9d24f47c9463c358ef43fdd23567965692b6f8
Qstick <qstick@gmail.com>
2023-05-06 13:43:29 -0500

    [Alias] Further Migration to discord.py 2.0
    Co-Authored-By: Jakub Kuczys <me@jacken.men>

--- aliasarr/__init__.py
+++ aliasarr/__init__.py
@@ -2,7 +2,5 @@ from .aliasarr import Aliasarr
 from redbot.core.bot import Red
 
 
-async def setup(bot: Red):
-    cog = Aliasarr(bot)
-    await bot.add_cog(cog)
-    cog.sync_init()
+async def setup(bot: Red) -> None:
+    await bot.add_cog(Aliasarr(bot))
