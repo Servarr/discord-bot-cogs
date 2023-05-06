b1612855d937a84b381eeb58d48d78812a1e716b
Qstick <qstick@gmail.com>
2023-05-06 15:24:13 -0500

    Fix Parserr Init

--- parserr/__init__.py
+++ parserr/__init__.py
@@ -2,4 +2,4 @@ from .parserr import Parserr
 from redbot.core.bot import Red
 
 async def setup(bot: Red) -> None:
-    await bot.add_cog(cog = Parserr(bot))
+    await bot.add_cog(Parserr(bot))
--- parserr/parserr.py
+++ parserr/parserr.py
@@ -12,7 +12,7 @@ from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 
 log = logging.getLogger("red.servarr.parserr")
 
-__version__ = "1.1.8"
+__version__ = "1.1.9"
 
 
 class Parserr(commands.Cog):
